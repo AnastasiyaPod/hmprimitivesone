111875bad92c166dc4ba92b601d0cb7b598c8f7e
AnastasiyaPod <an_pod12@mail.ru>
2023-08-19 22:44:16 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 13676;
+        int mile = 20;
+        int miles = priceTicket / mile;
+        System.out.println(miles);
+    }
+}
